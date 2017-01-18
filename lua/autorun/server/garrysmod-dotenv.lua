16889e276e8e679645039ef740b498b2436003c2
TheClonker <philip.gisella@googlemail.com>
2017-01-18 14:05:59 +0100

    Added Reload Command and Comments

--- lua/autorun/server/garrysmod-dotenv.lua
+++ lua/autorun/server/garrysmod-dotenv.lua
@@ -8,43 +8,53 @@ function setenv( id, data )
 	return true
 end
 
-local filename = hook.Call( "dotenvInitializing" ) or ".env" //Custom .env Filename
-ENV = {} //Global Enviroment Var
-if file.Exists( filename, "DATA" ) then
-	// Read the .env File
-	local rawdata = file.Read( filename, "DATA" )
-	// Split into a Table of all Lines
-	local dataTable = string.Explode( "\n", rawdata )
-	// Check that the File isn't empty
-	if table.Count( dataTable ) == 0 or string.Trim( rawdata ) == "" then
-		// File is empty
-		MsgC( Color( 137, 222, 255 ), "[dotenv] " , Color( 255, 0, 0 ), filename .. " is empty! Aborting!\r\n" )
-	else
-		// Trim the Stings and insert into global Table
-		for line, data in pairs( dataTable ) do
-			// Find first equals sign
-			local pos, _, _ = string.find( data, "=" )
-			// Check if there is an equals sing
-			if pos == nil and string.Trim( data ) != "" then
-				// No equals sing found
-				MsgC( Color( 137, 222, 255 ), "[dotenv] " , Color( 255, 0, 0 ), filename .. " is malformed at line #" .. line .. "! Skipping!\r\n" )
-			// Ingnore empty lines
-			elseif string.Trim( data ) != "" then
-				// Get Key and Value and trim the excess spaces
-				local key 	= string.Trim( string.sub( data, 0, pos - 1) )
-				local value = string.Trim( string.sub( data, pos + 1 ) )
-				// Insert into the global Table
-				ENV[key] = value
+function dotenv_init()
+	local filename = hook.Call( "dotenvInitializing" ) or ".env" //Custom .env Filename
+	ENV = {} //Global Enviroment Var
+	if file.Exists( filename, "DATA" ) then
+		// Read the .env File
+		local rawfile = file.Read( filename, "DATA" )
+		// Split into a Table of all Lines
+		local dataTable = string.Explode( "\n", rawfile )
+		// Check that the File isn't empty
+		if table.Count( dataTable ) == 0 or string.Trim( rawfile ) == "" then
+			// File is empty
+			MsgC( Color( 137, 222, 255 ), "[dotenv] " , Color( 255, 0, 0 ), filename .. " is empty! Aborting!\r\n" )
+		else
+			// Trim the Stings and insert into global Table
+			for line, rawdata in pairs( dataTable ) do
+				// Trim the Lines
+				local data = string.Trim( rawdata )
+				// Ignore Comments in the File
+				if string.sub( data, 1, 1 ) == "#" then continue end
+				// Find first equals sign
+				local pos, _, _ = string.find( data, "=" )
+				// Check if there is an equals sing
+				if pos == nil and string.Trim( data ) != "" then
+					// No equals sing found
+					MsgC( Color( 137, 222, 255 ), "[dotenv] " , Color( 255, 0, 0 ), filename .. " is malformed at line #" .. line .. "! Skipping!\r\n" )
+				// Ingnore empty lines
+				elseif string.Trim( data ) != "" then
+					// Get Key and Value and trim the excess spaces
+					local key 	= string.Trim( string.sub( data, 0, pos - 1) )
+					local value = string.Trim( string.sub( data, pos + 1 ) )
+					// Insert into the global Table
+					ENV[key] = value
+				end
 			end
+			//everything finished loading, calling hook
+			MsgC( Color( 137, 222, 255 ), "[dotenv] " , Color( 0, 255, 0 ),  filename .. " loaded!\r\n" )
+			hook.Call( "dotenvFinished" )
 		end
-		//everything finished loading, calling hook
-		MsgC( Color( 137, 222, 255 ), "[dotenv] " , Color( 0, 255, 0 ),  filename .. " loaded!\r\n" )
-		hook.Call( "dotenvFinished" )
+	else
+		// File not found
+		MsgC( Color( 137, 222, 255 ), "[dotenv] " , Color( 255, 0, 0 ), filename .. " not found! Aborting!\r\n" )
 	end
-else
-	// File not found
-	MsgC( Color( 137, 222, 255 ), "[dotenv] " , Color( 255, 0, 0 ), filename .. " not found! Aborting!\r\n" )
 end
+dotenv_init()
+
+// Adding a Reload Command
+concommand.Add( "dotenv_reload", dotenv_init )
 
 // Hook to overwrite the filename to load eg. production.env, development.env
 //hook.Add( "dotenvInitializing", "gmod-dotenv-changename", function() return "test.env" end)
